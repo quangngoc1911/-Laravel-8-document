8ec73b59c99925cbe79be3ba4abb423c9cfc1d79
NhatMinh-Kraken <nguyennhatminh26122001@gmail.com>
2022-12-12 20:01:49 +0700

    1

--- Web/app/Providers/AppServiceProvider.php
+++ Web/app/Providers/AppServiceProvider.php
@@ -23,10 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-
-        $this->registerPolicies();
- 
-        Passport::routes();
+        
     }
 }
