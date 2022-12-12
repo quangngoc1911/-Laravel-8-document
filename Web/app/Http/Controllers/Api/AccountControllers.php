984f19cd61a76d1603bdb82d307d7d8b6315648f
NhatMinh-Kraken <nguyennhatminh26122001@gmail.com>
2022-12-12 21:55:35 +0700

    2

--- Web/app/Http/Controllers/Api/AccountControllers.php
+++ Web/app/Http/Controllers/Api/AccountControllers.php
@@ -6,6 +6,25 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 
+use Illuminate\Support\Facades\Validator;
+
 class AccountControllers extends Controller{
-    
+    public function register (Request $request)
+    {
+        //validasi
+        // $validator = Validator::make($request->all(),[
+        //     'HoVaTen' => ['required','max:255'],
+        //     'TenDN'=>['required','max:255','unique:account'],
+        //     'Passwords'=>['required','max:255','confirmed']
+        // ],[
+        //     'HoVaTen.required' => 'Please enter your first and last name !!!',
+        //     'HoVaTen.max' => 'Character limit is 255 !!!',
+        //     'TenDN.required'=>'Please enter your account !!!',
+        //     'TenDN.unique'=> 'This account has already existed !!! ',
+        //     'TenDN.max'=>'Character limit is 255 !!!',
+        //     'Passwords.required'=>'Please enter your password !!!',
+        //     'Passwords.max'=>'Character limit is 255 !!!',
+        //     'Passwords.confirmed'=>'Character limit is 255 !!!',
+        // ])
+    }
 }
