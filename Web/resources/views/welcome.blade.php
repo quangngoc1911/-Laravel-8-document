1c7e89d8144aff55ede44004d54f583b3a6c1bf4
quangngoc1911 <87185694+quangngoc1911@users.noreply.github.com>
2022-12-17 22:34:03 +0700

    Update welcome.blade.php

--- Web/resources/views/welcome.blade.php
+++ Web/resources/views/welcome.blade.php
@@ -124,6 +124,12 @@
                         </div>
                     </div>
 
+
+
+hello
+
+
+
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
